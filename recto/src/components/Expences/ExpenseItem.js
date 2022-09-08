317bbb7aa1936d349ac009645be8b386459bdf78
Karn Tiwari <karntiwari5353@gmail.com>
2022-09-08 11:16:35 +0530

    ________

--- recto/src/components/Expences/ExpenseItem.js
+++ recto/src/components/Expences/ExpenseItem.js
@@ -1,11 +1,13 @@
+import React from 'react';
+
 import './ExpenseItem.css';
 
 import ExpenseDate from './ExpenseDate';
-
+import Card from '../UI/Card';
 function ExpenseItem(props){
   
     return (
-        <div className="expense-item">
+        <card className="expense-item">
             <ExpenseDate date = {props.date}/>
             <div className="expense-item__description">
                 <h2>
@@ -13,7 +15,7 @@ function ExpenseItem(props){
                 </h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
+        </card>
     )
 }
 
