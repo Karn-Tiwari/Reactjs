ff6b4ba4f007d56cf5e4e5446dc22839fa8906d4
Karn Tiwari <karntiwari5353@gmail.com>
2022-09-15 18:42:44 +0530

    new pages

--- recto/tecto/this/script.js
+++ recto/tecto/this/script.js
@@ -18,5 +18,32 @@
 // This keyword is used to access the properties of the current object
 // This keyword is used to call the function of the current object
 
+//METHOD :: Bind funtion
 
+// function fn(){
+//     console.log(this);
+//     function abc(){
+//         console.log(this);
+//     }
+//     let ret = abc.bind(this);
+//     ret();
+// }
+// let obj = {
+//     name:"karn",
+//     func:fn
+// }
+// obj.func();
 
+// METHOD :: Arrow function
+function fn(){
+    console.log(this);
+     abc=()=>{// arrow function ye karta hai ki usko jo (this) chahiye usko liye wo apne parents k pass jata hai aur utha leta hai
+        console.log(this);// jab yaha pe console.log(this) kiya gaya to directly abc()ko directly this mill gaya
+    }
+   abc();
+}
+let obj = {
+    name:"karn",
+    func:fn
+}
+obj.func();
