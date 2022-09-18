ec1fd51dcd20337bb9836e9e4d91f9eb7e03efde
Karn Tiwari <karntiwari5353@gmail.com>
2022-09-18 19:37:56 +0530

    _________

---
+++ todo/src/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Todo extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+export const arr = [1, 2, 3, 4, 5];
+export default Todo;
