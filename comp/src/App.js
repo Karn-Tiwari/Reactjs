2e578772924571e514e24ace31e5bb9f2e4eb89e
Karn Tiwari <karntiwari5353@gmail.com>
2022-12-28 03:34:53 +0530

    new commit

--- comp/src/App.js
+++ comp/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import Heading from './Heading';
+// import Para from './Para';
+// import List from './List';
+
+// function App()
+// {
+//   return(
+//     <>
+//     <Heading/>
+//     <Para/>
+//     <List/>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
 
 function App() {
+  let curDate = new Date();
+  curDate = curDate.getHours();
+  let greeting = "";
+  const cssStyle = {};
+  if (curDate >= 1 && curDate < 12) {
+    greeting = "Good Morning";
+    cssStyle.color = "Red";
+   cssStyle.backgroundColor="rgb(246, 246, 246)";
+  } else if (curDate >= 12 && curDate < 17) {
+    greeting = "Good Afternoon";
+    cssStyle.color = "brown";
+    cssStyle.backgroundColor="rgb(246, 246, 246)";
+  } else if(curDate>=17 && curDate <=24) {
+    greeting = "Good Night";
+    cssStyle.color = "green";
+    cssStyle.backgroundColor="rgb(246, 246, 246)";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="heading">
+        <h1 >
+          Hey Karn,<span style={cssStyle}>{greeting}</span>
+        </h1>
+      </div>
+    </>
   );
 }
-
 export default App;
---
+++ comp/src/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function List()
+{
+   return (<ol>
+    <li>Rewali</li>
+    <li>Rudauli</li>
+    <li>Ayodhya</li>
+    <li>Uttar Pradesh</li>
+    <li>India Computer Science and Engineering</li>
+    </ol>)
+}
+
+export default List;
